6da225772bb7735c404d290fba16c81c11d3259a
anniechakraborty <the.demented.genius29@gmail.com>
2023-03-27 13:14:52 +0530

    created basic navbar layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarHeaderComponent } from './components/navbar-header/navbar-header.component';
+import { MaterialModule } from './material.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarHeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
